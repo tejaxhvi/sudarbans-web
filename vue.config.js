3cedbdb20ad0bd375fa4e02af2adfce42c6f3802
Samar Ahmad <codewithsamsol@gmail.com>
2026-04-15 21:55:02 +0530

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  // transpileDependencies should be an array of dependency names to transpile.
-  // It was accidentally set to `true` which causes Vue CLI to call `.map` on it.
-  transpileDependencies: []
-})
