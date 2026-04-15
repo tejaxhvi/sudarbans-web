60457901d0b2e6def4caf33805f9d8c7b95fa01b
Samar Ahmad <codewithsamsol@gmail.com>
2026-04-15 21:46:45 +0530

    Delete tests/unit directory

--- tests/unit/HomePage.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import HomePage from '../../src/components/HomePage';
-
-describe('HomePage.vue', () => {
-  it('renders without errors', () => {
-    const wrapper = mount(HomePage);
-    expect(wrapper.exists()).toBe(true);
-  });
-});
